d67c468eb05740b88bf4fbff98aa7df062c44b6b
EBarrett233 <93392004+EBarrett233@users.noreply.github.com>
2022-02-01 10:31:58 -0500

    Add files via upload

---
+++ Koans work/basics.sql
@@ -0,0 +1,23 @@
+-- Meditate on upper-case queries
+SELECT 1
+
+-- Meditate on lower-case queries
+select 1
+
+-- Meditate on selecting all columns from a table
+select * from book
+
+-- Meditate on selecting one column (title) from a table
+select title from book
+
+-- Meditate on selecting some columns (title, available) from a table
+select title, available from book
+
+-- Meditate on adding a new book
+insert into book(title, genre, year_published, available)
+values('The maid', 'Mystery', 2022, 1)
+
+-- Meditate on adding two new books
+insert into book(title, genre, year_published, available)
+values('if you tell', 'Mystery', 2022, 1)
+,('Then she was gone', 'Mystery', 2022, 1)
